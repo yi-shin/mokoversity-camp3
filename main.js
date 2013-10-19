78e503f77f6986c8d25828353f8e0672ccf4a8c8
yi-shin <lindy41144@gmail.com>
2013-10-19 15:08:57 +0800

    Module Pattern,fix #7

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 
-(function (){
-	var canvas = document.getElementById('game'),
-		ctx = canvas.getContext('2d'),
-		ballX = Math.floor(Math.random()*300),
-		ballY = Math.floor(Math.random()*500),
-		ballR = Math.floor(Math.random()*100);
-	canvas.width = 480;
-	canvas.height = 320;
-	ctx.fillStyle ='pink';
-	ctx.beginPath();
-	ctx.arc(ballX,ballY,ballR,0,Math.PI*2,true);
-	ctx.fill();
+var gameModule = (function (){
+	function start(){
+		var canvas = document.getElementById('game'),
+			ctx = canvas.getContext('2d'),
+			ballX = Math.floor(Math.random()*300),
+			ballY = Math.floor(Math.random()*500),
+			ballR = Math.floor(Math.random()*100);
+		
+		canvas.width = 480;
+		canvas.height = 320;
+		ctx.fillStyle ='DeepSkyBlue ';
+		ctx.beginPath();
+		ctx.arc(ballX,ballY,ballR,0,Math.PI*2,true);
+		ctx.fill();
+	}
+	return {
+		start:start
+	}
 })();
+
+gameModule.start();
