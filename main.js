6d28385810a9bddbc9bf222b27400648d63401b7
yi-shin <lindy41144@gmail.com>
2013-10-19 14:42:41 +0800

    random,fix #6

---
+++ main.js
@@ -0,0 +1,14 @@
+
+(function (){
+	var canvas = document.getElementById('game'),
+		ctx = canvas.getContext('2d'),
+		ballX = Math.floor(Math.random()*300),
+		ballY = Math.floor(Math.random()*500),
+		ballR = Math.floor(Math.random()*100);
+	canvas.width = 480;
+	canvas.height = 320;
+	ctx.fillStyle ='pink';
+	ctx.beginPath();
+	ctx.arc(ballX,ballY,ballR,0,Math.PI*2,true);
+	ctx.fill();
+})();
